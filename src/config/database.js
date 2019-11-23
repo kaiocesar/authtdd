ccc761f9fa81ab38725682a4592885c274794b5b
Kaio Cesar <tecnico.kaio@gmail.com>
2019-11-23 12:25:53 -0300

    add sequelize

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+	host: '127.0.0.1',
+	username: 'root',
+	password: 'root',
+	database: 'nodeauth',
+	dialect: 'postgres',
+	operatorsAliases: false,
+	logging: false,
+	define: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true,
+	}
+}
---
+++ src/database/migrations/20191123151620-create-users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email:{
+        type: Sequelize.STRING,
+        
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
